dbbf8b9d110a6c158eff0710a530fea2c7ba01d5
s5935512064 <32189965+s5935512064@users.noreply.github.com>
2022-07-27 11:42:31 +0700

    eventpopup

--- tailwind.config.js
+++ tailwind.config.js
@@ -17,9 +17,9 @@ module.exports = {
       // => @media (min-width: 1024px) { ... }
 
       xl: "1280px",
-      // => @media (min-width: 1280px) { ... }
 
-      "2xl": "1536px",
+      // => @media (min-width: 1280px) { ... }
+      xxl: "1440px",
       // => @media (min-width: 1536px) { ... }
     },
   },
